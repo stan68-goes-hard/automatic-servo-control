e2c09c93df275b13fd7bac285edd4367a5a2fc8a
stan68-goes-hard <stanislavmalikov90@gmail.com>
2024-09-23 00:13:00 +0200

    Update GPIOServoControl.py
    pinout

--- GPIOServoControl.py
+++ GPIOServoControl.py
@@ -3,12 +3,10 @@ import time
 import numpy as np
 
 # Set up GPIO
-
+# (check if the pinout is correct)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)    # Servo 1    ???
-GPIO.setup(13, GPIO.OUT)    # Servo 2    ???
-servo1 = GPIO.PWM(11, 50)    # pin 11 for servo1 ???
-servo2 = GPIO.PWM(13, 50)    # pin 13 for servo2 ???
+GPIO.setup(11, GPIO.OUT)    # Servo 1   
+servo1 = GPIO.PWM(11, 50)    # pin 11 for servo1 
 
 servo1.start(0)
 
