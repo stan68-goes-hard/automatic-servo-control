3514f7bfc074c95d612009a9ba924053185399aa
stan68-goes-hard <stanislavmalikov90@gmail.com>
2024-09-29 21:55:59 +0200

    servo works, but it has jitters

--- PINs clear from RPi.GPIO.py
+++ PINs clear from RPi.GPIO.py
@@ -1,8 +1,21 @@
-import from RPi.GPIO as GPIO 
+from gpiozero import Servo
+from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.cleanup()
-print("PINs are reset now")
-input("Press Enter to exit...")
-if input is True:
-exit(0)
+servo = Servo(17)
+
+correction=0.5
+maxPW=(2.0+correction)/1000
+minPW=(1.0-correction)/1000
+
+
+servo.mid()
+sleep(1)
+servo.min()
+sleep(1)
+servo.max()
+sleep(1)
+servo.mid()
+sleep(1)
+
+
+servo.close()
